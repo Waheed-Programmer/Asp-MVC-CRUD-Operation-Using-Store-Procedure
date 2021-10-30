bc50f1593b5d4482337d8bfec02a3e0928ed7e57
WAHEED ARSHAD <70683576+Waheed-Programmer@users.noreply.github.com>
2021-10-31 00:58:53 +0500

    Add Connection String and connect data base in sql server management studio

--- MVC CRUD Operation Store Procedure/Models/Services/DAL.cs
+++ MVC CRUD Operation Store Procedure/Models/Services/DAL.cs
@@ -1,5 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 
@@ -7,5 +10,31 @@ namespace MVC_CRUD_Operation_Store_Procedure.Models.Services
 {
     public class DAL
     {
+        SqlConnection con = new SqlConnection(ConfigurationManager.ConnectionStrings["CS"].ConnectionString);
+        SqlCommand cmd;
+        SqlDataAdapter sd;
+        DataTable dt;
+
+        public List<EmployeModel> GetEmloye()
+        {
+            cmd = new SqlCommand("sp_view", con);
+            cmd.CommandType = CommandType.StoredProcedure;
+            sd = new SqlDataAdapter(cmd);
+            dt = new DataTable();
+            sd.Fill(dt);
+            List<EmployeModel> list = new List<EmployeModel>();
+            foreach(DataRow dr in dt.Rows)
+            {
+                list.Add(new EmployeModel
+                {
+                    Id = Convert.ToInt32(dr["Id"]),
+                    Name = dr["Name"].ToString(),
+                    Eamil = dr["Email"].ToString(),
+                    Age = Convert.ToInt32(dr["Name"])
+                });
+               
+            }
+            return list;
+        }
     }
 }
