721728ad6953fb2cc3260a55416e68664749740d
WAHEED ARSHAD <70683576+Waheed-Programmer@users.noreply.github.com>
2021-10-31 00:34:50 +0500

    Create Employe model class

---
+++ MVC CRUD Operation Store Procedure/Models/EmployeModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVC_CRUD_Operation_Store_Procedure.Models
+{
+    public class EmployeModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Eamil { get; set; }
+        public int Age { get; set; }
+    }
+}
